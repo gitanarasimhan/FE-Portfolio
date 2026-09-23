4580b803fd9ddd8a4c9d63bd0b2f2cde3b13996e
Gita Narasimhan <n.geeta@gmail.com>
2026-09-23 14:20:18 -0500

    Add DragDropArea component stories for Storybook

---
+++ src/components/DragDropArea/DragDropArea.stories.tsx
@@ -0,0 +1,25 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { DragDropArea } from './DragDropArea';
+
+const meta: Meta<typeof DragDropArea> = {
+  title: 'Components/DragDropArea',
+  component: DragDropArea,
+  args: {
+    accept: 'image/*',
+    onFiles: (files) => console.log('files:', files),
+  },
+};
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: (args) => <DragDropArea {...args} />,
+};
+
+export const AnyFileType: Story = {
+  args: {
+    accept: '*',
+  },
+  render: (args) => <DragDropArea {...args} />,
+};
