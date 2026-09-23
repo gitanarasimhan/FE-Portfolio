ecd5f9981a82f6b54d98589fa3cb6725f090b047
Gita Narasimhan <n.geeta@gmail.com>
2026-09-23 14:36:31 -0500

    Refactor Storybook main configuration

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,20 +1,11 @@
-import type { Meta, StoryObj } from '@storybook/react';
-import { Carousel } from './Carousel';
-
-const meta = {
-  title: 'Components/Carousel',
-  component: Carousel,
-} satisfies Meta<typeof Carousel>;
-
-export default meta;
-
-type Story = StoryObj<typeof meta>;
-
-export const Default: Story = {
-  render: () => (
-    <Carousel>
-      <div>Slide 1</div>
-      <div>Slide 2</div>
-    </Carousel>
-  ),
+module.exports = {
+  stories: [
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../src/**/*.mdx',
+  ],
+  addons: [],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
 };
